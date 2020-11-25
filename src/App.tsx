cfcad23bca4bb0ddaca4c35e42ef6a0e329ed546
carlijnweijer <carlijnweijer@gmail.com>
2020-11-25 22:00:09 +0100

    Set up React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi i'm app</h1>
+      <Switch>
+        <Route exact path="/" />
+        <Route exact path="/signup" />
+        <Route exact path="/login" />
+      </Switch>
     </div>
   );
 }
