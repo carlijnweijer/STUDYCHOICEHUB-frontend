09d88a03ffff9c3ea7629345ef34db8f6ffcd869
carlijnweijer <carlijnweijer@gmail.com>
2020-11-26 14:17:59 +0100

    Add Login Form with NO functionality

--- src/pages/LoginSignup/LoginPage.tsx
+++ src/pages/LoginSignup/LoginPage.tsx
@@ -1,9 +1,58 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+} from "@material-ui/core";
+
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 export default function LoginPage() {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const onLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    console.log("i got clicked");
+    event.preventDefault();
+  };
+
   return (
     <div>
       <h1>I'm the login page</h1>
+      <form noValidate autoComplete="off" onSubmit={onLogin}>
+        <div>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="component-outlined">Email</InputLabel>
+            <OutlinedInput
+              id="component-outlined"
+              value={email}
+              onChange={handleEmailChange}
+              label="E-mail"
+            />
+          </FormControl>
+          <FormControl variant="outlined">
+            <InputLabel htmlFor="component-outlined">Password</InputLabel>
+            <OutlinedInput
+              id="component-outlined"
+              value={password}
+              onChange={handlePasswordChange}
+              label="Password"
+            />
+          </FormControl>
+          <Button color="primary" variant="outlined" type="submit">
+            Login
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
