3b6251ea30d5f65f461109adc92f172ce684d00d
carlijnweijer <carlijnweijer@gmail.com>
2020-11-28 12:20:25 +0100

    Add sidebar and align items properly

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,32 @@ import LogoutPage from "./pages/LoginSignup/LogoutPage";
 import SignupPage from "./pages/LoginSignup/SignupPage";
 import StudyPage from "./pages/Study/StudyPage";
 import Homepage from "./pages/HomePage/Homepage";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <UploadButton />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/signup" component={SignupPage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/logout" component={LogoutPage} />
-        <Route path="/study/:id" component={StudyPage} />
-      </Switch>
+    <div className="app">
+      <div className="topBar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <div className="sideBar">
+          <Sidebar />
+        </div>
+        <div className="content">
+          <UploadButton />
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/signup" component={SignupPage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/logout" component={LogoutPage} />
+            <Route path="/study/:id" component={StudyPage} />
+          </Switch>
+        </div>
+      </div>
+      <div className="footer">
+        <div>i'm footer</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div>
+      <p>sidebar</p>
+      {
+        //map through categories
+      }
+    </div>
+  );
+}
