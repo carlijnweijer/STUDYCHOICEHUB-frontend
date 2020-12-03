333af8d15199fd4235f22dcbbaca4eb3842ec30a
carlijnweijer <carlijnweijer@gmail.com>
2020-12-03 16:19:44 +0100

    Linked navbar brand to home

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { selectToken } from "../../store/user/selectors";
 import LoggedIn from "./LoggedIn";
 import LoggedOut from "./LoggedOut";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -29,9 +30,15 @@ export default function Navbar() {
     <div className={classes.root}>
       <AppBar position="static" color="transparent">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            STUDYCHOICE HUB
-          </Typography>
+          <Link to="/" style={{ textDecoration: "none", flex: 1 }}>
+            <Typography
+              variant="h6"
+              className={classes.title}
+              color="textPrimary"
+            >
+              STUDYCHOICE HUB
+            </Typography>
+          </Link>
           {loginLogoutControls}
         </Toolbar>
       </AppBar>
